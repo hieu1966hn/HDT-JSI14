f205007658592314d8f7123a64e030aa557c7ff0
hieu1966hn <hieu1966hn@gmail.com>
2022-10-02 10:33:11 +0700

    Kế thừa, done khai báo Person

--- SS3/oop.js
+++ SS3/oop.js
@@ -38,28 +38,52 @@ Vai trò?
 // }
 
 /////////// Thuộc tính của Class
+// class Person {
+//   /// Hàm khởi tạo: Khai báo thuộc tính 
+//   constructor(name, age, address, id) {
+//     // Gán giá trị  với class
+//     this._name = name;
+//     this._age = age;
+//     this._address = address;
+//     this._id = id;
+//   }
+
+//   say() {
+//     alert("Tôi đang nói đấy")
+//   }
+
+//   coding() {
+//     alert("Đang code đấy đừng động vào")
+//   }
+// }
+
+
+// let Hieu = new Person('Hiếu', 25, "Hà Nội", "0123456789");
+// // console.log("Hieu: ", Hieu);
+// console.log(Hieu._address);
+// Hieu.coding()
+
+
+
+/*
+Bốn tính chất của OOP:
+1. Đóng gói
+2. Đa hình
+3. Kế thừa
+4. Trừu tượng
+
+*/
+
+///// Tính kế thừa
 class Person {
-  /// Hàm khởi tạo: Khai báo thuộc tính 
-  constructor(name, age, address, id) {
-    // Gán giá trị  với class
-    this._name = name;
-    this._age = age;
-    this._address = address;
-    this._id = id;
+  constructor(_name, _age, _address) {
+    this.name = _name;
+    this.age = _age;
+    this.address = _address;
+
   }
 
   say() {
-    alert("Tôi đang nói đấy")
-  }
-
-  coding() {
-    alert("Đang code đấy đừng động vào")
+    return `I'am Person and my name is ${this.name}`
   }
 }
-
-
-let Hieu = new Person('Hiếu', 25, "Hà Nội", "0123456789");
-// console.log("Hieu: ", Hieu);
-console.log(Hieu._address);
-Hieu.coding()
-
