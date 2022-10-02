23cf968fd6140d5ef380e2f9ed298212ec04d467
hieu1966hn <hieu1966hn@gmail.com>
2022-10-02 10:16:23 +0700

    done khai báo phương thức Class

---
+++ SS3/oop.js
@@ -0,0 +1,40 @@
+/// OOP là gì? Vai trò của OOP trong lập trình?
+/*
+OOP là gì?
+- Phúc Anh: 
+- Nhật Minh: Lập trình hướng đối tượng là pp lập trình dựa trên các khái niệm class và Object
+- Bảo Lâm: 
+
+Vai trò?
+- Nhật Minh: Tăng khả năng tái sử dụng
+- Bảo Lâm: 
+*/
+
+
+//////// Cú pháp OOP trong JS
+//// Khởi tạo class trong JS
+//// Khai báo Người
+// class Person {
+
+//   say() {
+//     alert("Hello, How are you!!")
+//   }
+// }
+
+// let MinhTien = new Person();
+// MinhTien.say()
+
+/// Ví dụ chưa có kiểu dữ liệu array
+// class Array1{
+//   filter(){
+//     return []
+//   }
+
+//   map(){
+//     return []
+//   }
+
+  
+// }
+
+/////////// Thuộc tính của Class
