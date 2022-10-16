6cc2c5c524aba613eda8aa99ce6b17fb8de9e597
hieu1966hn <hieu1966hn@gmail.com>
2022-10-16 09:42:28 +0700

    Done bài 10

--- SS5/app.js
+++ SS5/app.js
@@ -1,9 +1,9 @@
 /*
-Buổi 5: 
+Buổi 5:
 1. Chữa bài tập checkpoint: [CRUD, OOP]
 2. Học về firebase authentication (Tạo tk firebase, cách nhúng firebase vào dự án)
 
-Chữa bài checkpoint 
+Chữa bài checkpoint
 
 I. Trắc nghiệm:
 1.D
@@ -18,17 +18,35 @@ I. Trắc nghiệm:
 
 II. Thực hành
 
-Câu 9: Cho array m = [1,2,3,4,5,6,”hh”, “9”,80,100]. 
-1. Hãy thực hiện xóa những phần tử không phải dạng number. 
-2. Sau đó, tạo ra một array mới tên là mSquared thỏa mãn: 
+Câu 9: Cho array m = [1,2,3,4,5,6,”hh”, “9”,80,100].
+1. Hãy thực hiện xóa những phần tử không phải dạng number.
+2. Sau đó, tạo ra một array mới tên là mSquared thỏa mãn:
 mSquared[i] = pow(m[i],2).
-*/
-let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100]
 
-/// Xóa 1 phần tử không phải dạng number 
-let n = m.filter(item => typeof (item) == "number");
-let mSquared = n.map(item => item * item)
-console.log("mSquared: ", mSquared);
+Câu 10: Thực hiện duyệt string sau đây: “ High knowledge, high return”.
+Chuyển đổi string về dạng array, xóa bỏ các phần tử có giá trị là “ “;
+và các phần tử có giá trị là chữ hoa chuyển thành chữ thường.
+
+
+*/
+
+////// chữa bài câu 9
+// let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100]
+
+// /// Xóa 1 phần tử không phải dạng number 
+// let n = m.filter(item => typeof (item) == "number");
+// let mSquared = n.map(item => item * item)
+// console.log("mSquared: ", mSquared);
+
+
+/////// Câu 10: chữa
+let str = " High knowledge, high return";
+let strToArray = str.toLowerCase();
+console.log("strToArray: ", strToArray);
+strToArray = strToArray.split(" ");
+console.log("strToArray: ", strToArray);
+strToArray = strToArray.filter(item => item != "")
+console.log("strToArray: ", strToArray);
 
 
 
