271678eadcfd564e498136d0f864fded08b8c1b6
hieu1966hn <hieu1966hn@gmail.com>
2022-10-16 10:37:26 +0700

    NEW CODE BÀI 12

--- SS5/app.js
+++ SS5/app.js
@@ -59,7 +59,7 @@ Phương thức trong class People gồm: set(), get() là hàm nhập và xuấ
 Tạo class Students kế thừa từ class People.
 Class Students sẽ có thêm:
 Thuộc tính id để lưu mã sinh viên, math lưu điểm môn toán, physical để lưu điểm môn vật lý, chemistry để lưu điểm môn hoá học.
-Phương thức: set(), get(), GPA() để tính điểm trung bình 3 môn học  
+Phương thức: set(), get(), GPA() để tính điểm trung bình 3 môn học
 
 
 */
@@ -67,60 +67,106 @@ Phương thức: set(), get(), GPA() để tính điểm trung bình 3 môn họ
 
 ///// Chữa bài 11: 
 
-class People {
-  constructor(name, age, address) {
-    this.name = name;
-    this.age = age;
-    this.address = address
-  }
+// class People {
+//   constructor(name, age, address) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address
+//   }
 
-  set(prop, value) {
-    this[prop] = value
-    return this[prop]
-  }
+//   set(prop, value) {
+//     this[prop] = value
+//     return this[prop]
+//   }
 
-  get(prop) {
-    return this[prop]
-  }
+//   get(prop) {
+//     return this[prop]
+//   }
 
+// }
+
+// let MinhTien = new People("Minh Tiến", 18, "Hà Nội");
+
+// MinhTien.set("name", "Trung Hiếu");
+// console.log("MinhTien: ", MinhTien);
+// console.log(
+//   MinhTien.get("age")
+// );
+
+
+// ///// Tạo 1 class Student kế thừa từ class cha
+// class Student extends People {
+//   constructor(name, age, address, id, math, physical, chemistry) {
+//     super(name, age, address); /// Kế thừa từ cha nó 3 thuộc tính
+//     this.id = id;
+//     this.math = math;
+//     this.physical = physical;
+//     this.chemistry = chemistry;
+//   }
+
+//   // Get và set đều đã được kế thừa từ cha
+
+//   GPA() {
+//     /////// convert từ string => number
+//     if (typeof (this.math + this.physical + this.chemistry) !== "number") {
+//       return "Score must be number"
+//     }
+
+//     const arr = [this.math, this.physical, this.chemistry];
+//     const total = arr.reduce((a, current) => a + current); // tổng 3 cái
+//     const average = total / arr.length;
+//     return average;
+//   }
+
+// }
+
+// let student = new Student("John", 24, "Hà Nội", "0123456789", 10, 5, 10);
+// console.log(student);
+
+
+
+
+/*
+Bài 12: 
+Viết một chương trình định nghĩa một lớp hình có constructor
+đưa ra giá trị của chiều rộng và chiều dài. 
+Sau đó, định nghĩa hai lớp con hình tam giác và hình chữ nhật. 
+Hai lớp con này tính diện tích của hình. 
+Trong lớp chính, định nghĩa hai biến hình tam giác và hình chữ nhật. 
+Sau đó gọi hàm area() trong hai biến này.
+
+*/
+
+class Shape {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 }
 
-let MinhTien = new People("Minh Tiến", 18, "Hà Nội");
-
-MinhTien.set("name", "Trung Hiếu");
-console.log("MinhTien: ", MinhTien);
-console.log(
-  MinhTien.get("age")
-);
-
-
-///// Tạo 1 class Student kế thừa từ class cha
-class Student extends People {
-  constructor(name, age, address, id, math, physical, chemistry) {
-    super(name, age, address); /// Kế thừa từ cha nó 3 thuộc tính
-    this.id = id;
-    this.math = math;
-    this.physical = physical;
-    this.chemistry = chemistry;
+class Triangle extends Shape {
+  constructor(width, height) {
+    super(width, height);
   }
-
-  // Get và set đều đã được kế thừa từ cha
-
-  GPA() {
-    /////// convert từ string => number
-    if (typeof (this.math + this.physical + this.chemistry) !== "number") {
-      return "Score must be number"
-    }
-
-    const arr = [this.math, this.physical, this.chemistry];
-    const total = arr.reduce((a, current) => a + current); // tổng 3 cái
-    const average = total / arr.length;
-    return average;
+  area() {
+    // Trả về diện tích tam giác
+    return (this.width * this.height) / 2;
   }
-
 }
 
-let student = new Student("John", 24, "Hà Nội", "0123456789", 10, 5, 10);
-console.log(student);
+class Rectangle extends Shape {
+  constructor(width, height) {
+    super(width, height);
+  }
 
+  area() {
+    // trả về diện tích hình chữ nhật
+    return this.width * this.height;
+  }
+}
 
+/// THỬ VỚI HÌNH TAM GIÁC
+// let tamgiac = new Triangle(20, 40)
+// console.log("tamgiac: ", tamgiac);
+
+/// TƯƠNG TỰ VỚI HÌNH CHỮ NHẬT
