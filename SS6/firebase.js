065667607e9e31b0b97e04441f65272d7bcc4e5a
hieu1966hn <hieu1966hn@gmail.com>
2022-10-23 10:56:02 +0700

    done 1. Set up firebase.js 2. Code Register.html 3. Done code auth.css

--- SS6/firebase.js
+++ SS6/firebase.js
@@ -1,1 +1,16 @@
-h
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeFhOd852yBEPfwpWznfEpdjgdwCxE-Ck",
+  authDomain: "mindx-blog.firebaseapp.com",
+  projectId: "mindx-blog",
+  storageBucket: "mindx-blog.appspot.com",
+  messagingSenderId: "1027591204225",
+  appId: "1:1027591204225:web:ea7114f55d8ce921110c80",
+  measurementId: "G-0ET9PD22BS"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
