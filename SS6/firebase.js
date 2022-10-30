6176cc8122ef86f59c39c2931ad03be8a68f147c
hieu1966hn <hieu1966hn@gmail.com>
2022-10-30 10:04:45 +0700

    done firebase setup

--- SS6/firebase.js
+++ SS6/firebase.js
@@ -1,6 +1,7 @@
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCeFhOd852yBEPfwpWznfEpdjgdwCxE-Ck",
   authDomain: "mindx-blog.firebaseapp.com",
@@ -14,3 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const subscription = [];
+
